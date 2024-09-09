82a81f51640bc10d95580cdb25db3cc19e63b27a
elfinix <axllouis.coronel@gmail.com>
2024-09-09 11:10:55 +0800

    Update to current

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
+import RandomNumberGenerator from "./pages/RandomNumberGenerator";
+import RandomWordGenerator from "./pages/RandomWordGenerator";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<LandingPage />} />
+                <Route path="/random-number-generator" element={<RandomNumberGenerator />} />
+                <Route path="/random-word-generator" element={<RandomWordGenerator />} />
+            </Route>
+        )
+    );
 
-export default App
+    return <RouterProvider router={router} />;
+};
+
+export default App;
